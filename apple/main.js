bd9df218ef348ed929fb8514d6056eae40fab6a7
agustinurioste <76100677+agustinurioste@users.noreply.github.com>
2021-02-23 01:45:33 -0500

    yes

--- apple/main.js
+++ apple/main.js
@@ -2,10 +2,10 @@ const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
 
-const frameCount = 174;
+const frameCount = 342;
 const currentFrame = index => (
  //`https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
- `https://raw.githubusercontent.com/agustinurioste/agustinurioste.github.io/main/assets/sequence/${index.toString().padStart(5, '0')}.png`
+ `https://raw.githubusercontent.com/agustinurioste/agustinurioste.github.io/main/assets/oop/${index.toString().padStart(5, '0')}.jpg`
  )
 
 const preloadImages = () => {
@@ -27,7 +27,6 @@ const updateImage = index => {
   img.src = currentFrame(index);
   context.drawImage(img, 0, 0);
 }
-
 
 
 window.addEventListener('scroll', () => {  
